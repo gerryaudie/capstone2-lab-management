e4909975c888ab4bb316b09f7eabc559fe6adbd8
gerryaudie <91952007+gerryaudie@users.noreply.github.com>
2026-06-08 23:42:59 +0700

    Setup database connection and environment configuration

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
 const express = require('express');
+const db = require('./config/db');
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Capstone Lab Management');
 });
 
-app.listen(3000, () => {
-    console.log('Server berjalan di port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server berjalan di port ${process.env.PORT}`);
 });
